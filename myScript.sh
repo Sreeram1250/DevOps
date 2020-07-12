7f6623557b2d2b0a810232253e6c72a7f07829f7
Sreeram1250 <sree.kar1250@gmail.com>
2020-07-12 14:00:25 +1000

    devops

---
+++ myScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PH=$(curl -sL http://169.254.169.254/latest/meta-data/public-hostname)
+PIP=$(curl -sL http://169.254.169.254/latest/meta-data/public-ipv4)
+sg=$(curl -sL http://169.254.169.254/latest/meta-data/security-groups)
+Ins=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
+InsType=$(curl -sL http://169.254.169.254/latest/meta-data/instance-type)
+echo "==================================="
+echo "public host name is : $PH"
+echo "Public ip is :$PIP"
+echo "Security group $sg"
+echo "Instance id is $Ins"
+echo "Instance type is $InsType"
